009a243a4fee7ec171dbf23a5f09c3660794e80e
ThiagoPeixoto81 <thiagopeixoto81xd@gmail.com>
2023-05-15 11:53:04 -0300

    tests

--- src/navigations/AppSignNavigation.js
+++ src/navigations/AppSignNavigation.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/Login/Login';
-import Register from '../screens/Register/index';
+import Register from '../screens/Register/Register';
 
 const Stack = createNativeStackNavigator();
 
--- src/screens/Register/__test__/Register.test.js
+++ src/screens/Register/__test__/Register.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react-native';
-import Register from '../index';
+import Register from '../Register';
 
 describe('Register', () => {
     test('teste para saber se o componente renderizou', () => {
